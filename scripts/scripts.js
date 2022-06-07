ed0472c2e8626304c142d54eb69ced399b6e7e3a
n.iskakov <n.iskakov@platform.live>
2022-06-07 16:58:51 +0200

    #2 added save button

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,11 +1,14 @@
 let countEl = document.getElementById("count-el")
 let count = 0;
+let saveEl = document.getElementById("save-el")
 
 function increment(){
-    count = ++count;
+    count += 1;
     countEl.innerText = count;
 }
 
 function save(){
-    console.log(count);
+    saveEl.textContent += count + ' - ';
+    countEl.textContent=0;
+    count = 0;
 }
